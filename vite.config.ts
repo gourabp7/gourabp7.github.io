cc89c264bb049af595271d1ca1bb3ef2296d801d
Gourab Panigrahi <pgourab@iisc.ac.in>
2025-11-08 15:31:55 +0530

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,13 @@
+// Polyfill localStorage for Node builds (prevent @typescript/vfs error)
+if (typeof (globalThis as any).localStorage === 'undefined') {
+  (globalThis as any).localStorage = {
+    getItem: (_k: string) => null,
+    setItem: (_k: string, _v: string) => {},
+    removeItem: (_k: string) => {},
+    clear: () => {}
+  }
+}
+
 import { sveltekit } from '@sveltejs/kit/vite'
 import { SvelteKitPWA as pwa } from '@vite-pwa/sveltekit'
 // @ts-expect-error ts(7016)
